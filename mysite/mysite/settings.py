989cf6a7ef0cd35af8e2c9d313a572d620bfbba8
Ziyi Wang <zw3917@nyu.edu>
2024-02-29 19:46:50 -0500

    deploy4

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
